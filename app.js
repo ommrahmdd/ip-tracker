b08b5d3d40c5ad58418ac836e92aec72b8f93c5b
omar <ommrahmdd@gmail.com>
2022-07-14 14:44:07 +0200

    .

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ let userLocation = document.querySelector("#location");
 let timezone = document.querySelector("#timezone");
 let isp = document.querySelector("#isp");
 let counter = 0;
+
 searchBtn.addEventListener("click", function () {
-  fetch(
-    `https://geo.ipify.org/api/v2/country?apiKey=at_Qa91aW05B4rR9FzFGjllUKXHkvmx7&ipAddress=${searchInput.value}`
-  )
+  let url = searchInput.value.includes("www")
+    ? `https://geo.ipify.org/api/v2/country,city?apiKey=at_Qa91aW05B4rR9FzFGjllUKXHkvmx7&domain=${searchInput.value}`
+    : `https://geo.ipify.org/api/v2/country,city?apiKey=at_Qa91aW05B4rR9FzFGjllUKXHkvmx7&ipAddress=${searchInput.value}`;
+  fetch(url)
     .then((res) => {
       return res.json();
     })
@@ -17,31 +19,21 @@ searchBtn.addEventListener("click", function () {
       userLocation.textContent = `${data.location.country}-${data.location.region}`;
       timezone.textContent = data.location.timezone;
       isp.textContent = data.isp;
-      searchInput.value = "";
       return fetch(`https://ipapi.co/${searchInput.value}/json/`);
     })
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      searchInput.value = "";
+      if (!data.latitude || !data.longitude) alert("Can't find the position");
       counter++;
       defineMap(data.latitude, data.longitude);
     });
 });
 var map;
 function defineMap(lat, long) {
-  let option = {
-    center: [lat, long],
-    zoom: 17,
-  };
-  // if (map != undefined) {
-  //   console.log("hey");
-  //   map.remove();
-  // }
   if (counter == 1) {
-    console.log("Greater");
     map = L.map("map").setView([lat, long], 15);
   } else {
-    console.log(map);
     map.setView([lat, long], 15);
   }
   let layer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
